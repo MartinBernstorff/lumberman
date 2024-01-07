43420050f810d0bad2e6b85708d6d4449140e1c4
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-07 17:34:28 +0100

    $fix: misc. types (#17)
    $fix: misc. types
    
    Fixes #14
    
    $fix: misc. types
    
    Fixes #14
    
    refactor: cleanup
    
    misc.

--- build/lib/herbarium/test_issue_service.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from .issue_service import GithubIssueService
-
-
-@pytest.mark.skip()
-def test_issue_service():
-    service = GithubIssueService()
-    service.get_issues_assigned_to_me()
