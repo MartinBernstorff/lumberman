3e4bd48c54d214ea520ac85ed80bd33002893d68
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-07 16:28:49 +0000

    refactor: cleanup

--- herbarium/issue_presenter.py
+++ herbarium/issue_presenter.py
@@ -2,10 +2,7 @@ from collections.abc import Sequence
 from typing import Protocol
 
 from .issue_service import Issue
-from .string_parsing import (
-    get_letter_alphabet_position,
-    get_letter_from_alphabet_position,
-)
+from .string_parsing import get_letter_alphabet_position, get_letter_from_alphabet_position
 
 
 class IssuePresenter(Protocol):
@@ -16,7 +13,7 @@ class IssuePresenter(Protocol):
 class DefaultIssuePresenter(IssuePresenter):
     def select_issue_dialog(self, issues: Sequence[Issue]) -> Issue:
         issue_strings = [
-            f"[{get_letter_from_alphabet_position(i+1)}] #{issue.entity_id} {issue.title}"
+            f"[{get_letter_from_alphabet_position(i+1)}] {issue.title}"
             for i, issue in enumerate(issues)
         ]
 
@@ -25,15 +22,11 @@ class DefaultIssuePresenter(IssuePresenter):
         terminal_output = "\n".join(reversed(issue_strings))
         print(f"\n{terminal_output}\n")
 
-        print(
-            "I found these issues for you. Which do you want to work on?\n"
-        )
+        print("I found these issues for you. Which do you want to work on?\n")
 
         issue_index = (
             get_letter_alphabet_position(
-                input(
-                    f"[a-{get_letter_from_alphabet_position(n_issues)}]: "
-                )
+                input(f"[a-{get_letter_from_alphabet_position(n_issues)}]: ")
             )
             - 1
         )
