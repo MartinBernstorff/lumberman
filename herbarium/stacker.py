158d1047ed061f8f470fffda8f6d1e7e6bfc4e37
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-07 17:25:53 +0000

    misc.

--- herbarium/stacker.py
+++ herbarium/stacker.py
@@ -45,7 +45,7 @@ class Graphite(Stacker):
     def add_to_stack(self, issue: Issue):
         sanitised_title = sanitise_issue_title(issue.title)
         branch_title = f"{issue.entity_id}-{sanitised_title}"
-        first_commit_str = f"$'{issue.title}\n\nFixes #{issue.entity_id}'"
+        first_commit_str = f"'{issue.title}\n\nFixes #{issue.entity_id}'"
         interactive_cmd(f"gt create {branch_title} --all -m {first_commit_str}")
         interactive_cmd(f"git commit --allow-empty -m {first_commit_str}")
 
