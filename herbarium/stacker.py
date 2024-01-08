60c9a3268bd55b69af6510fdfa4f6776bd45b6c6
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-08 13:39:40 +0000

    misc.

--- herbarium/stacker.py
+++ herbarium/stacker.py
@@ -43,12 +43,17 @@ class Stacker(Protocol):
 
 
 class Graphite(Stacker):
+    def _sync(self):
+        interactive_cmd("gt sync --force --show-delete-progress")
+    
     def create_stack_from_trunk(self, issue: Issue):
+        self._sync()
         interactive_cmd("git checkout main")
         interactive_cmd("git pull")
         self.add_to_stack(issue)
 
     def add_to_stack(self, issue: Issue):
+        self._sync()
         sanitised_title = sanitise_issue_title(issue.title)
         branch_title = f"{issue.entity_id}-{sanitised_title}"
         first_commit_str = f"'{issue.title}\n\nFixes #{issue.entity_id}'"
@@ -56,6 +61,7 @@ class Graphite(Stacker):
         interactive_cmd(f"git commit --allow-empty -m {first_commit_str}")
 
     def submit_stack(self, automerge: bool):
+        self._sync()
         submit_command = "gt submit --no-edit --publish"
 
         if automerge:
