a4300f9d9779cfa9e7566850a32e65a33d737bbf
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-08 14:50:00 +0000

    misc.

--- herbarium/issue_presenter.py
+++ herbarium/issue_presenter.py
@@ -1,5 +1,5 @@
 from collections.abc import Sequence
-from typing import Protocol
+from typing import Optional, Protocol
 
 import questionary
 from rich.console import Console
@@ -10,16 +10,23 @@ console = Console()
 
 
 class IssuePresenter(Protocol):
-    def select_issue_dialog(self, issues: Sequence[Issue]) -> Issue:
+    def select_issue_dialog(self, issues: Sequence[Issue], refresh_prompt: str) -> Optional[Issue]:
         ...
 
 
 class DefaultIssuePresenter(IssuePresenter):
-    def select_issue_dialog(self, issues: Sequence[Issue]) -> Issue:
-        selected_issue_title = questionary.select(
+    def _show_dialog(self, issues: Sequence[Issue], refresh_prompt: str) -> Optional[str]:
+        issue_titles = [issue.title for issue in issues]
+        return questionary.select(
             "I found these issues for you. Which one do you want to work on?",
-            choices=[issue.title for issue in issues],
+            choices=[refresh_prompt, *issue_titles],
+            default=issue_titles[0],
             qmark="",
         ).ask()
+
+    def select_issue_dialog(self, issues: Sequence[Issue], refresh_prompt: str) -> Optional[Issue]:
+        selected_issue_title = self._show_dialog(issues=issues, refresh_prompt=refresh_prompt)
+        if selected_issue_title == refresh_prompt:
+            return None
 
         return next(issue for issue in issues if issue.title == selected_issue_title)
