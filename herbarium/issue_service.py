302d7fe19db0e62e15326649617c9b9e04cf6709
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-09 13:35:58 +0000

    misc.

--- herbarium/issue_service.py
+++ herbarium/issue_service.py
@@ -3,13 +3,15 @@ from collections.abc import Sequence
 from dataclasses import dataclass
 from typing import Optional, Protocol
 
+from .parse_issue_title import parse_issue_title
 from .subprocess_utils import shell_output
 
 
 @dataclass(frozen=True)
 class Issue:
     entity_id: Optional[str]
-    title: str
+    prefix: str
+    description: str
 
 
 class IssueService(Protocol):
@@ -25,6 +27,14 @@ class GithubIssueService(IssueService):
     def setup(self) -> None:
         pass
 
+    def _values_to_issue(self, values: dict[str, str]) -> Issue:
+        parsed_title = parse_issue_title(values["title"])
+        return Issue(
+            entity_id=str(values["number"]),
+            description=parsed_title.description,
+            prefix=parsed_title.prefix,
+        )
+
     def get_issues_assigned_to_me(self) -> Sequence[Issue]:
         """Get issues assigned to current user on current repo"""
         my_issues_cmd = shell_output("gh issue list --assignee='@me' --json number,title")
@@ -33,6 +43,7 @@ class GithubIssueService(IssueService):
             return []
 
         values = json.loads(my_issues_cmd)
-        parsed_output = [Issue(entity_id=str(v["number"]), title=v["title"]) for v in values]
+
+        parsed_output = [self._values_to_issue(v) for v in values]
 
         return parsed_output
