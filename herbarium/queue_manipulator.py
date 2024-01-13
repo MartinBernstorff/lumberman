a3ca5c44eef75143ddfc1c5495c573bbbe2b7fcd
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-13 15:58:40 +0000

    misc.

--- herbarium/queue_manipulator.py
+++ herbarium/queue_manipulator.py
@@ -1,10 +1,13 @@
 from typing import Protocol
 
 from .issue_parser import IssueParser
+from .issue_service import Issue
+from .subprocess_utils import interactive_cmd
 
 
 class QueueManipulator(Protocol):
     issue_parser: IssueParser
+    issue: Issue
 
     def fork(self):
         ...
@@ -15,12 +18,18 @@ class QueueManipulator(Protocol):
 
 class GraphiteManipulator(QueueManipulator):
     issue_parser: IssueParser
+    issue: Issue
+
+    def __post_init__(self):
+        self.issue_info = self.issue_parser.get_issue_info(issue=self.issue)
 
     def fork(self):
-        ...
+        interactive_cmd(
+            f'gt create {self.issue_info.branch_title} --all -m "{self.issue_info.first_commit_str}"'
+        )
 
     def add(self):
-        ...
-        # Handle base case
-
-        # Handle insertion case
+        interactive_cmd(
+            f'gt create {self.issue_info.branch_title} --all --insert -m "{self.issue_info.first_commit_str}"'
+        )
+        interactive_cmd(f'git commit --allow-empty -m "{self.issue_info.first_commit_str}"')
