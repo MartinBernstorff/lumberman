2726f8519c3ee2137e872f673b5a1c38dfc4f886
Martin Bernstorff <martinbernstorff@gmail.com>
2024-01-13 16:12:10 +0000

    misc.

--- herbarium/registry.py
+++ herbarium/registry.py
@@ -2,11 +2,11 @@ from herbarium import queue_manipulator
 
 from .issue_presenter import DefaultIssuePresenter, IssuePresenter
 from .issue_service import GithubIssueService, IssueService
-from .queue_navigator import QueueNavigator
+from .queue_navigator import GraphiteNavigator, QueueNavigator
 
 issue_services: dict[str, type[IssueService]] = {"Github": GithubIssueService}
 
-queue_navigators: dict[str, type[QueueNavigator]] = {}
+queue_navigators: dict[str, type[QueueNavigator]] = {"Graphite": GraphiteNavigator}
 queue_manipulators: dict[str, type[queue_manipulator.QueueManipulator]] = {
     "Graphite": queue_manipulator.GraphiteManipulator
 }
